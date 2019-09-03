f2c05c0724733fc10e0af2f408ab70e2a4e434b0
akash parajuli <akash.parajuli001@gmail.com>
2019-09-03 14:59:54 -0500

    added unit test

---
+++ NUnitTestProject1/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication2.Test;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+    }
+}
---
+++ WebApplication2/Test/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Test
+{
+    public class Calculator : ICalculator
+    {
+
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+    }
+}
